149c04f506a96b2b122f15c58d4c8f63f5520627
Satyam Sharma <satyamsharmamec102@gmail.com>
2024-11-18 15:53:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const quizRouter = require("./router/quiz.router");
+const {loginRouter, signupRouter} = require("./router/auth.router");
+const routeNotFound = require("./middleware/routeNotFound");
+
+
+const app = express();   // Creating a Server
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello Geeks");
+});
+
+app.use("/quiz", quizRouter);
+app.use("/auth/login", loginRouter);
+app.use("/auth/signup", signupRouter);
+app.use(routeNotFound); 
+
+
+
+// app.get("/quiz", (req, res) => {
+//     res.send(quizzes.data);
+// })
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log("Server Started");
+})
